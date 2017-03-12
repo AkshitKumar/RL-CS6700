9988c4144506049a36b4027b3a696f5f0ea6cec0
Anveshak IITM <anveshak@Anveshak.Server>
2017-03-12 06:20:09 +0530

    Add files for goal B experiment

---
+++ Programming Assignment 2/QLearning/experiment_goal_b.py
@@ -0,0 +1,55 @@
+import sys
+import math
+import numpy as np
+import rlglue.RLGlue as RLGlue
+
+num_of_agents = 50
+num_of_episode = 1000
+goal_states = ['B']
+
+def evaluateAgent(eval_return,eval_num_of_step):
+	calc_return = 0
+	calc_num_steps = 0	
+	n = 10
+	RLGlue.RL_agent_message("freeze learning")
+	RLGlue.RL_agent_message("freeze exploring")
+	for i in range(0,n):
+		RLGlue.RL_episode(0)
+		calc_return += float(RLGlue.RL_env_message("Return"))
+		calc_num_steps += int(RLGlue.RL_env_message("num_of_steps"))
+		print "EVALUATION Episode : " + str(i) + " RETURN : " + RLGlue.RL_env_message("Return") + " STEPS : " + RLGlue.RL_env_message("num_of_steps")
+	RLGlue.RL_agent_message("unfreeze exploring")
+	RLGlue.RL_agent_message("unfreeze learning")
+	calc_return /= n
+	calc_num_steps /= n
+	eval_return.append(calc_return)
+	eval_num_of_step.append(calc_num_steps)
+
+def saveEvaluation(eval_return,eval_num_of_step,num):
+	eval_num_of_step = np.array(eval_num_of_step)
+	eval_return = np.array(eval_return)
+	np.save('eval_goal_b/eval_return_agent_' + str(num),eval_return)
+	np.save('eval_goal_b/eval_num_steps_agent_' + str(num),eval_num_of_step)
+
+for i in range(len(goal_states)):
+	for j in range(num_of_agents):
+		RLGlue.RL_init()
+		RLGlue.RL_env_message("set-goal-" + goal_states[i])
+		returns = []
+		num_steps = []
+		eval_return = []
+		eval_num_of_step = []
+		for episode in range(num_of_episode):
+			RLGlue.RL_episode(0)
+			returns.append(float(RLGlue.RL_env_message("Return")))
+			num_steps.append(RLGlue.RL_num_steps())
+			print 'Episode No: ' + str(episode + 1) + " Agent No: " + str(j + 1) + ' Goal :' + goal_states[i] + " Completed" + " Return : " + RLGlue.RL_env_message("Return") + "  Steps to Goal : " + RLGlue.RL_env_message("num_of_steps")
+			if episode == num_of_episode - 1:
+				RLGlue.RL_agent_message("save_policy result_goal_B_agent_" + str(j+1) + ".dat") # Save the value function for the agent
+			if (episode % 25 == 0):
+				evaluateAgent(eval_return,eval_num_of_step)
+		returns = np.array(returns)
+		num_steps = np.array(num_steps)		
+		np.save('returns_goal_b/return_' + 'agent_' + str(j) + '_goal_' + goal_states[i],returns)
+		np.save('num_steps_goal_b/num_step_' + 'agent_' + str(j) + '_goal_' + goal_states[i],num_steps)
+		saveEvaluation(eval_return,eval_num_of_step,j)
